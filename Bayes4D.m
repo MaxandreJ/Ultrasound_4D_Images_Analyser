fd1b8f00fed3af445e3bd6e0e12fde3aadf2a29f
MaxandreJ <maxandre.jacqueline@me.com>
2016-07-29 17:59:42 +0200

    Gestion des erreurs et amélioration du sous-échantillonnage

--- Bayes4D.m
+++ Bayes4D.m
@@ -1434,34 +1434,81 @@ function sous_echantillonnage_Callback(hObject, eventdata, handles)
 % hObject    handle to sous_echantillonnage (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-valeur_nombre_de_pics = str2double(get(handles.valeur_nombre_de_pics,'String'));
+
+nombre_de_pics = str2double(get(handles.valeur_nombre_de_pics,'String'));
 choix_ROI_polygone = handles.choix_ROI_polygone ;
 graphique_selon_axe4_choisi = get(handles.graphique_selon_axe4,'value');
+ordre_axes = handles.ordre_axes;
 
 
-sous_echantillonnage_possible = (valeur_nombre_de_pics==1) && choix_ROI_polygone && ...
-    graphique_selon_axe4_choisi;
-if sous_echantillonnage_possible
-    %t_maximum = handles.abscisse_courbe_ROI(2);
-    %a changer
-    t_maximum=16;
+%sous_echantillonnage_possible = (valeur_nombre_de_pics==1) && choix_ROI_polygone && ...
+%    graphique_selon_axe4_choisi;
+%if sous_echantillonnage_possible
+try
+    axe_abscisse_pas_temps = ~graphique_selon_axe4_choisi || ordre_axes(4)~=4;
+    if nombre_de_pics ~= 1
+        erreur_trop_de_pics.message = 'Le nombre de pics détectés est strictement supérieur à 1.';
+        erreur_trop_de_pics.identifier = 'sous_echantillonnage_Callback:trop_de_pics';
+        error(erreur_trop_de_pics);
+    elseif ~choix_ROI_polygone
+        erreur_polygone_pas_choisi.message = 'La région d''intérêt n''a pas été choisie avec un polygone.';
+        erreur_polygone_pas_choisi.identifier = 'sous_echantillonnage_Callback:polygone_pas_choisi';
+        error(erreur_polygone_pas_choisi);
+    elseif axe_abscisse_pas_temps
+        erreur_axe_abscisse_pas_temps.message = 'L''axe des abscisses du graphique affiché n''est pas le Temps.';
+        erreur_axe_abscisse_pas_temps.identifier = 'sous_echantillonnage_Callback:axe_abscisse_pas_temps';
+        error(erreur_axe_abscisse_pas_temps);
+    end
+        
+    t_maximum=handles.abscisse_courbe_ROI(end);
     t_du_maximum_global = handles.x_maxs(1); 
-    [nom_du_fichier,chemin] = uiputfile({'*.mat'});
+    [prefixe_des_fichiers,chemin_sauvegarde] = uiputfile({'*.*'});
+    choix_annulation = isequal(prefixe_des_fichiers,0) || isequal(chemin_sauvegarde,0);
+    if choix_annulation
+        erreur_choix_annulation.message = 'L''utilisateur a annulé son action de sauvegarde.';
+        erreur_choix_annulation.identifier = 'sous_echantillonnage_Callback:choix_annulation';
+        error(erreur_choix_annulation);
+    end
     dossier_principal=pwd;
-    cd(chemin);
+    cd(chemin_sauvegarde);
+    barre_attente = waitbar(0,'Merci de patienter pendant l''enregistrement des fichiers...');
     for t=1:t_maximum
         condition_echantillonnage_normal = t<(3/2)*t_du_maximum_global;
         if condition_echantillonnage_normal
             volume_a_enregistrer=handles.volumes(:,:,:,t);
             volume_a_enregistrer=squeeze(volume_a_enregistrer);
-            save(['test',num2str(t),'.mat'],'volume_a_enregistrer');
+            save([prefixe_des_fichiers,num2str(t),'.mat'],'volume_a_enregistrer');
         elseif mod(t,2)==0
             volume_a_enregistrer=handles.volumes(:,:,:,t);
             volume_a_enregistrer=squeeze(volume_a_enregistrer);
-            save(['test',num2str(t),'.mat'],'volume_a_enregistrer');
+            save([prefixe_des_fichiers,num2str(t),'.mat'],'volume_a_enregistrer');
         end
+        waitbar(t/t_maximum);
     end
-    cd(dossier_principal)
+    cd(dossier_principal);
+    close(barre_attente);
+catch erreurs
+    if (strcmp(erreurs.identifier,'sous_echantillonnage_Callback:trop_de_pics'))
+        warndlg('Merci de choisir de détecter un seul pic à l''étape précédente.');
+        causeException = MException(erreur_trop_de_pics.identifier,erreur_trop_de_pics.message);
+        erreurs = addCause(erreurs,causeException);
+        throw(causeException);
+    elseif (strcmp(erreurs.identifier,'sous_echantillonnage_Callback:polygone_pas_choisi'))
+        warndlg('Merci de choisir une région d''intérêt de forme polygonale et de recommencer les étapes jusqu''ici.');
+        causeException = MException(erreur_polygone_pas_choisi.identifier,erreur_polygone_pas_choisi.message);
+        erreurs = addCause(erreurs,causeException);
+        throw(causeException);
+    elseif (strcmp(erreurs.identifier,'sous_echantillonnage_Callback:axe_abscisse_pas_temps'))
+        warndlg('Merci de choisir comme axe des abscisse le temps à l''étape ''affichage du graphique''.');
+        causeException = MException(erreur_axe_abscisse_pas_temps.identifier,erreur_axe_abscisse_pas_temps.message);
+        erreurs = addCause(erreurs,causeException);
+        throw(causeException);
+    elseif (strcmp(erreurs.identifier, 'sous_echantillonnage_Callback:choix_annulation'))
+        causeException = MException(erreur_choix_annulation.identifier,erreur_choix_annulation.message);
+        erreurs = addCause(erreurs,causeException);
+    else
+        rethrow(erreurs);
+    end   
 end
     
     
