2935cf971c7f832b78ad5d0dad813824bf1c31ad
MaxandreJ <maxandre.jacqueline@me.com>
2016-08-07 17:52:04 +0200

    Ajout de la classe sous-echantillonnage

---
+++ Code/Sous_echantillonnage.m
@@ -0,0 +1,15 @@
+classdef Sous_echantillonnage
+    %SOUS_ECHANTILLONNAGE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        vecteur_t_ech_normal
+        vecteur_t_ssech
+        sauvegarde
+    end
+    
+    methods
+    end
+    
+end
+
