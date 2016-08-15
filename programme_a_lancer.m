9626f097e4d01078966a82f2540652eaae5b4619
MaxandreJ <maxandre.jacqueline@me.com>
2016-08-15 17:37:56 +0200

    Changement des noms des variables dans le programme_a_lancer (main)

--- programme_a_lancer.m
+++ programme_a_lancer.m
@@ -2,29 +2,29 @@ function programme_a_lancer()
 %Merci de lancer ce script pour lancer le programme.
 
 %% Ajout du répertoire Modèle au chemin de Matlab
-code = fullfile(pwd,'Modele');
-chemin_code = genpath(code);
-addpath(chemin_code);
+chemin = fullfile(pwd,'Modele');
+chemin_matlab = genpath(chemin);
+addpath(chemin_matlab);
 
 %% Ajout du répertoire Vue au chemin de Matlab
-code = fullfile(pwd,'Vue');
-chemin_code = genpath(code);
-addpath(chemin_code);
+chemin = fullfile(pwd,'Vue');
+chemin_matlab = genpath(chemin);
+addpath(chemin_matlab);
 
 %% Ajout du répertoire Controleur au chemin de Matlab
-code = fullfile(pwd,'Controleur');
-chemin_code = genpath(code);
-addpath(chemin_code);
+chemin = fullfile(pwd,'Controleur');
+chemin_matlab = genpath(chemin);
+addpath(chemin_matlab);
 
 %% Ajout du répertoire Interface Homme Machine au chemin de Matlab
-code = fullfile(pwd,'Interface Homme Machine');
-chemin_code = genpath(code);
-addpath(chemin_code);
+chemin = fullfile(pwd,'Interface Homme Machine');
+chemin_matlab = genpath(chemin);
+addpath(chemin_matlab);
 
 %% Ajout du répertoire altmany-export_fig au chemin de Matlab
-code = fullfile(pwd,'altmany-export_fig');
-chemin_code = genpath(code);
-addpath(chemin_code);
+chemin = fullfile(pwd,'altmany-export_fig');
+chemin_matlab = genpath(chemin);
+addpath(chemin_matlab);
 
 %% Lancement du programme
 modele = Modele;
